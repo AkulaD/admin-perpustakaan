b0b183f243dd8a983c3c0bb0cc92edf8141423df
shaka <sh4k4175@gmail.com>
2026-01-08 18:32:51 +0700

    08-01-2026 V2D

--- data/js/script.js
+++ data/js/script.js
@@ -6,49 +6,13 @@ toggle.addEventListener('click', () => {
 });
 
 
-// transaction.html
-let transactions = [
-    {
-        nama: "Peminjaman Buku Algoritma",
-        jumlah: 1,
-        kategori: "Peminjaman"
-    },
-    {
-        nama: "Peminjaman Buku Java",
-        jumlah: 1,
-        kategori: "Peminjaman"
-    }
-];
-
-const transactionBody = document.getElementById("transactionBody");
 const transactionForm = document.getElementById("transactionForm");
-
-function renderTransactions() {
-    transactionBody.innerHTML = "";
-
-    transactions.forEach((trx, index) => {
-        transactionBody.innerHTML += `
-            <tr>
-                <td>${index + 1}</td>
-                <td>${trx.nama}</td>
-                <td>${trx.jumlah}</td>
-                <td>${trx.kategori}</td>
-                <td>
-                    <a href="#" onclick="markDone(${index})">
-                        ${trx.kategori === "Pengembalian" ? "Selesai" : "Dikembalikan"}
-                    </a>
-                </td>
-                <td><a href="#" onclick="deleteTransaction(${index})">Hapus</a></td>
-            </tr>
-        `;
-    });
-}
 
 transactionForm.addEventListener("submit", function (e) {
     e.preventDefault();
 
-    const nama = document.getElementById("namaTransaksi").value;
-    const jumlah = document.getElementById("jumlah").value;
+    const nama = document.getElementById("namaTransaksi").value.trim();
+    const jumlah = document.getElementById("jumlah").value.trim();
     const kategori = document.getElementById("kategori").value;
 
     if (nama === "" || jumlah === "" || kategori === "") {
@@ -56,26 +20,7 @@ transactionForm.addEventListener("submit", function (e) {
         return;
     }
 
-    transactions.push({
-        nama,
-        jumlah,
-        kategori
-    });
+    alert("Transaksi berhasil disimpan!");
 
-    renderTransactions();
     transactionForm.reset();
 });
-
-function deleteTransaction(index) {
-    if (confirm("Yakin ingin menghapus transaksi ini?")) {
-        transactions.splice(index, 1);
-        renderTransactions();
-    }
-}
-
-function markDone(index) {
-    transactions[index].kategori = "Pengembalian";
-    renderTransactions();
-}
-
-renderTransactions();
